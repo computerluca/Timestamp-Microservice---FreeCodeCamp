d96f6f48b5d8b1246307d6a8b0d98eca97ff0415
Luca Traversari <trave5@yahoo.it>
2016-08-07 12:21:48 +0000

    Added possibility to add a parameters to the web service: if the user enters a unix timestamp or a date return the current date and the timestamp. Otherwise, the web service returns a null json object

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+/*I can pass a string as a parameter,
+and it will check to see whether that string 
+contains either a unix timestamp or a natural 
+language date (example: January 1, 2016)
+*/
+app.get("/",function(req,res){
+  
+  res.end( "<h1>API Basejump: Timestamp microservice</h1><p>For more information visit this link <a href='https://timestamp-ms.herokuapp.com/'>link</a></P>");
+  
+  
+})
+app.get('/:namet', function (req, res) {
+  var stringa = req.params.namet;
+  if(!(isNaN(stringa))){
+    var controlla = moment.unix(req.params.namet);
+    if(controlla.isValid()){
+    var dateString = moment.unix(req.params.namet).format("MMMM D, YYYY");
+    var oggetto = {"unix":req.params.namet,"natural":dateString};
+      res.send(oggetto);
+}
+else{
+  var oggetto = {"unix":null,"natural":null};
+  res.send(oggetto);
+}
+  }
+  else{
+    var controlla = moment(req.params.namet);
+    if(controlla.isValid()){
+    var dateString = (new Date(req.params.namet).getTime())/1000;
+    var oggetto ={"unix":dateString,"natural":req.params.namet};
+    res.send(oggetto);
+  }
+  else{
+    var oggetto = {"unix":null,"natural":null};
+  res.send(oggetto);
+  }
+  }
+  
 });
 
 app.listen(8080, function () {
